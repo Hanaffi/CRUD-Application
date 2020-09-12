870ab8146910dee50e62779703543550cb4159c7
Ahmed <ahmedellord56@gmail.com>
2020-09-12 20:08:09 +0200

    Updates

---
+++ precache-manifest.cb020b94665370d0524b154ea4b1e10c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc30e797753cd8abaf81daed4b328c4",
+    "url": "/CRUD-Application/index.html"
+  },
+  {
+    "revision": "2f491e713a2763a90b2e",
+    "url": "/CRUD-Application/static/css/main.5554cba5.chunk.css"
+  },
+  {
+    "revision": "fe485f721bb0c2ecf8d0",
+    "url": "/CRUD-Application/static/js/2.ed612737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CRUD-Application/static/js/2.ed612737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f491e713a2763a90b2e",
+    "url": "/CRUD-Application/static/js/main.dd557d46.chunk.js"
+  },
+  {
+    "revision": "a84e11eff9355149135e",
+    "url": "/CRUD-Application/static/js/runtime-main.1cd1b786.js"
+  }
+]);
